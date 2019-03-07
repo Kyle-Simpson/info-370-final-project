98aa7d79f42cfbed74e2966490bc1578a1622900
amelia-shull <ashull@uw.edu>
2019-03-07 14:01:21 -0800

    questions to answer through plots

--- data_exploration.py
+++ data_exploration.py
@@ -5,3 +5,19 @@ Created on Thu Mar  7 13:46:20 2019
 @author: ameli
 """
 
+# Food insecurity all specific to children: 
+
+# What does food insecurity look like on a map?
+
+# How does food insecurity relate to income and race?
+
+# How much food insecurity is there?
+
+# How do types and density of stores correlate with food insecurity?
+# ie more junk food -> food insecurity?
+
+# How does the population to area ratio affect food insecurity?
+
+# How do food programs relate to food insecurity?
+
+# How does adult food insecurity relate to children food insecurity?
