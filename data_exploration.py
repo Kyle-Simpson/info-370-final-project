86c4d3db31a96ecfa0dbbf175d49cbdeaf536f4d
Kateka Seth <katekaseth@gmail.com>
2019-03-08 13:22:33 -0800

    remove some collumns

--- data_exploration.py
+++ data_exploration.py
@@ -8,35 +8,35 @@ Created on Thu Mar  7 13:46:20 2019
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly as py
-import plotly.figure_factory as ff
-import geopandas
-import shapely
+import seaborn as sns
 
 data = pd.read_csv("data/prepped/compiled_data.csv")
-
+data = data.drop(["FOODINSEC_CHILD_03_11", "FOODINSEC_CHILD_01_07", "PERCHLDPOV10", "PERPOV10", "FOODINSEC_10_12",
+                  "VLFOODSEC_10_12", "PCT_LACCESS_HHNV10", "PCT_NHBLACK10", "FOODINSEC_13_15", "VLFOODSEC_13_15",
+                  "MILK_PRICE10", "REDEMP_WICS08", "PCT_NHNA10", "SNAP_CAP09", "PCT_LACCESS_LOWI10", "PCT_HISP10",
+                  "ORCHARD_ACRES07", "FIPS", "FIPS"], 1)
 # Food insecurity all specific to children: 
 
 # What does food insecurity look like on a map?
 
 # How does food insecurity relate to income and race?
-plt.figure(figsize=(20,20))
+plt.figure(figsize=(10,10))
 plt.scatter(data.MEDHHINC15, data.Food_Insec)
 plt.title("Median household income versus food insecurity")
 plt.show()
 
-plt.figure(figsize=(20,20))
+plt.figure(figsize=(10,10))
 plt.scatter(data.POVRATE15, data.Food_Insec)
 plt.title("Poverty rate versus food insecurity")
 plt.show()
 
-plt.figure(figsize=(20,20))
+plt.figure(figsize=(10,10))
 plt.scatter(data.PCT_NHWHITE10, data.Food_Insec)
 plt.title("Percent white versus food insecurity")
 plt.show()
 
 # Snap versus food insecurity
-plt.figure(figsize=(20,20))
+plt.figure(figsize=(10,10))
 plt.scatter(data.REDEMP_SNAPS16, data.Food_Insec)
 
 corr = data.corr()[['Food_Insec', 'Food_Insec_Children']]
@@ -51,22 +51,4 @@ corr = data.corr()[['Food_Insec', 'Food_Insec_Children']]
 # How do food programs relate to food insecurity?
 
 # How does adult food insecurity relate to children food insecurity?
-#%%
 
-colorscale = ["#f7fbff","#ebf3fb","#deebf7","#d2e3f3","#c6dbef","#b3d2e9","#9ecae1",
-              "#85bcdb","#6baed6","#57a0ce","#4292c6","#3082be","#2171b5","#1361a9",
-              "#08519c","#0b4083","#08306b"]
-endpts = list(np.linspace(1, 12, len(colorscale) - 1))
-fips = data['FIPS'].tolist()
-values = data['Food_Insec_Children'].tolist()
-
-fig = ff.create_choropleth(
-    fips=fips, values=values,
-    binning_endpoints=endpts,
-    colorscale=colorscale,
-    show_state_data=False,
-    show_hover=True, centroid_marker={'opacity': 0},
-    asp=2.9, title='Child Food Insecurity',
-    legend_title='Child Food Insecuirty %'
-)
-py.offline.plot(fig, filename='choropleth_full_usa')
